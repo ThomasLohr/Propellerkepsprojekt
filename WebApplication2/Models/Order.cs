c29ec60b91cb0a56115ddaa7b537facd2442833d
Johan Rova <johan.rova@protonmail.com>
2021-03-25 12:19:15 +0100

    Added AdminController, as well as two models, order and user(dummy class), also added views for Orders and Users.

---
+++ WebApplication2/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Order
+    {
+        public string CustomerName { get; set; }
+        public DateTime OrderTime { get; set; }
+        public int ItemAmount { get; set; }
+        public bool OrderSent { get; set; }
+    }
+}
---
+++ WebApplication2/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
