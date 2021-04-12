1494318d8ff660a322764007eff1d4f2ceae0942
DESKTOP-7GPI486\kseni <ksenia.gladkikh@outlook.com>
2021-04-12 11:41:28 +0200

    Added Unittest for SearchProduct

---
+++ PropStoreTests1/Services/ProductServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Services.Tests
+{
+    [TestClass()]
+    public class ProductServiceTests
+    {
+        ProductService productService = new ProductService();
+        [TestMethod()]
+        public void KeyWordEmptyTest()
+        { 
+            string tryString = " ";
+            productService.SearchProducts(tryString);
+            Assert.IsNotNull(string.Empty);
+        }
+        [TestMethod()]
+        public void KeyWordNullTest()
+        {
+            string tryString = null;
+            productService.SearchProducts(tryString);
+            Assert.IsNull(tryString);
+        }
+        [TestMethod()]
+        public void KeyWordIsEqualProductName()
+        {
+            string tryString = "hund";
+            productService.SearchProducts(tryString);
+            Assert.AreEqual(tryString, "hund");
+        } 
+        [TestMethod()]
+        public void KeyWordIsEqualModel()
+        {
+            string tryString = "Standard";
+            productService.SearchProducts(tryString);
+            Assert.AreEqual(tryString, "Standard");
+        }
+
+
+    }
+}
