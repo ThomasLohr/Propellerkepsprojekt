e0604b92ca23285fdb517d103708f6da108b823f
gustafrydell <73111186+gustafrydell@users.noreply.github.com>
2021-04-12 20:09:07 +0200

    Changed to correct language on errormessage for the productmodel

--- WebApplication2/Models/Product.cs
+++ WebApplication2/Models/Product.cs
@@ -10,28 +10,30 @@ namespace WebApplication2.Models
     public class Product : IModelDates
     {
         public int Id { get; set; }
-        [Required(ErrorMessage="Please enter a product name.")]
-        [MaxLength(50, ErrorMessage="Maximum 50 characters allowed.")]
+        [Required(ErrorMessage="Ange produktnamn.")]
+        [MaxLength(50, ErrorMessage="Max 50 bokstäver är tillåtet.")]
         public string ProductName { get; set; }
-        [Required]
-        [MaxLength(300, ErrorMessage = "Maximum 300 characters allowed.")]
+        [Required(ErrorMessage = "Ange produktbeskrivning.")]
+        [MaxLength(300, ErrorMessage = "Max 300 bokstäver är tillåtet.")]
         public string ProductDescription { get; set; }
-        [Required(ErrorMessage = "Please enter a price.")]
+        [Required(ErrorMessage = "Ange pris.")]
         public decimal Price { get; set; }
-
+        [Required(ErrorMessage = "Ange rabatt.")]
         public decimal Discount { get; set; }
 
+        [Required(ErrorMessage = "Ange bildUrl.")]
         public string ImageUrl { get; set; }
-        [Required(ErrorMessage = "Please enter a model.")]
+        [Required(ErrorMessage = "Ange modell.")]
         public string Model { get; set; }
-        [Required(ErrorMessage = "Please enter a size.")]
+        [Required(ErrorMessage = "Ange storlek.")]
         public string Size { get; set; }
-        [Required(ErrorMessage = "Please enter a gender.")]
+        [Required(ErrorMessage = "Ange kön.")]
         public string Gender { get; set; }
-        [Required(ErrorMessage = "Please enter a color.")]
+        [Required(ErrorMessage = "Ange färg.")]
         public string Color { get; set; }
-        [Required(ErrorMessage = "Please enter a stock amount.")]
+        [Required(ErrorMessage = "Ange lagersaldo.")]
         public int Stock { get; set; }
+        [Required(ErrorMessage = "Ange kategori.")]
         public string Category { get; set; }
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public virtual DateTime? CreatedDate { get; set; } = DateTime.UtcNow;
